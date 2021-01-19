44e5480b4842164afa0ab7e638c94ba932152157
Андрей <andrei.bagan2@mail.ru>
2021-01-19 09:46:22 +0300

    Добавлен способ входа/регистрации пользователей. Налажена система разграничения доступа по ролям

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentOffice.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base (options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string adminRoleName = "admin";
+            string userRoleName = "user";
+            string studentRoleName = "student";
+            string teacherRoleName = "teacher";
+
+            string adminEmail = "admin@mail.ru";
+            string adminPassword = "12345";
+
+            Role adminRole = new Role { Id = 1, Name = adminRoleName };
+            Role userRole = new Role { Id = 2, Name = userRoleName };
+            Role studentRole = new Role { Id = 3, Name = studentRoleName };
+            Role teacherRole = new Role { Id = 4, Name = teacherRoleName };
+
+            User adminUser = new User { id = 1, Email = adminEmail, Password = adminPassword, RoleId = adminRole.Id };
+
+            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole, studentRole, teacherRole });
+            modelBuilder.Entity<User>().HasData(new User[] { adminUser });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
